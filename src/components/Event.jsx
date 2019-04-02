72ae30b75e2e1a65c76d48497e2cbead007e4006
Jonathan Gescheit <jonathan.ges@gmail.com>
2019-04-02 17:34:18 +0300

    simple card

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 import { css, jsx } from '@emotion/core'
 
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
+
 
 const Event = ({ event }) => (
 	<Card className="card" css={css`
@@ -12,8 +14,9 @@ const Event = ({ event }) => (
 		height: 200px;
 		width: 200px;
 	`}>
+		<CardHeader title={event.title} />
 		<CardContent>
-			{event.title}
+			<span>{`id: ${event.id}`}</span>
 		</CardContent>
 	</Card>
 );
