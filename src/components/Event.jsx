e928d4958d996e0904fc0959d556eb34b971dc5f
Jonathan Gescheit <jonathan.ges@gmail.com>
2019-04-02 17:25:55 +0300

    Add emotion and material ui

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,21 @@
+/** @jsx jsx */
+
 import React from 'react';
+import { css, jsx } from '@emotion/core'
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const Event = ({ event }) => (
-	<div className="event">
-		{event.title}
-	</div>
+	<Card className="card" css={css`
+		padding-top: 20px;
+		height: 200px;
+		width: 200px;
+	`}>
+		<CardContent>
+			{event.title}
+		</CardContent>
+	</Card>
 );
 
 export default Event;
