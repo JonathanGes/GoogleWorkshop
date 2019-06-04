af38396f714e06d8c3cb6deaba9fb784f6587662
Jonathan Gescheit <jonathan.ges@gmail.com>
2019-06-04 11:57:07 +0300

    Grid

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -15,9 +15,11 @@ import Fab from "@material-ui/core/Fab";
 import { Animate } from "react-simple-animate";
 import DateAndTimePickers from "./DateAndTimePickers";
 import DeleteIcon from "@material-ui/icons/Delete";
-import Tasks from './Tasks';
-import Playlist from './Playlist';
-
+import Tasks from "./Tasks";
+import Playlist from "./Playlist";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
 
 const styles = theme => ({
   propertyBox: {
@@ -27,7 +29,7 @@ const styles = theme => ({
     marginLeft: "auto !important"
   },
   button: {
-    margin: theme.spacing.unit
+    marginRight: "8px"
   },
   input: {
     display: "none"
@@ -67,6 +69,11 @@ const styles = theme => ({
   },
   eventDetails: {
     // padding: "0 24px 0 30%"
+  },
+  editableInput: {
+    backgroundColor: theme.palette.primary,
+    borderRadius: "2px",
+    transition: "backgroundColor 200ms ease-in-out"
   }
 });
 
@@ -100,10 +107,26 @@ class Event extends Component {
 
     // eventStore.selectedEvent.addTask({ title: "Order sushi", id: "1" });
     console.log(eventStore.selectedEvent.tasks);
-    eventStore.selectedEvent.addPlaylistTrack({id: '1', title: 'wow', artist: 'oh'});
-    eventStore.selectedEvent.addPlaylistTrack({id: '2', title: 'omg', artist: 'eminem'});
-    eventStore.selectedEvent.addPlaylistTrack({id: '3', title: 'super', artist: 'drake'});
-    eventStore.selectedEvent.addPlaylistTrack({id: '4', title: 'last nite', artist: 'strokes'});
+    eventStore.selectedEvent.addPlaylistTrack({
+      id: "1",
+      title: "SAD!",
+      artist: "XXXTENTACION"
+    });
+    eventStore.selectedEvent.addPlaylistTrack({
+      id: "2",
+      title: "Hunnybee",
+      artist: "Unknown Mortal Orchestra"
+    });
+    eventStore.selectedEvent.addPlaylistTrack({
+      id: "3",
+      title: "Still Beating",
+      artist: "Mac Demarco"
+    });
+    eventStore.selectedEvent.addPlaylistTrack({
+      id: "4",
+      title: "Your DOg",
+      artist: "Soccer Mommy"
+    });
   }
 
   @action
@@ -118,90 +141,133 @@ class Event extends Component {
 
     return (
       <div className={classes.event}>
-        <div className="action-buttons" className={classes.actionButtons}>
-          <FormDialog />
-          <Button
-            variant="contained"
-            className={classes.button}
-            color="primary"
-          >
-            Save
-          </Button>
-          <Button
-            variant="contained"
-            className={classes.button}
-            color="secondary"
-          >
-            Publish
-          </Button>
-        </div>
-
-        <div className={classes.eventDetails}>
-          <div className={classes.propertyBox}>
-            <Typography variant="h6" color="primary">
-              Title
-            </Typography>
-            <RIEInput
-              value={eventStore.selectedEvent.title}
-              change={({ title }) => eventStore.selectedEvent.setTitle(title)}
-              propName="title"
-            />
-          </div>
-
-          <div className={classes.propertyBox}>
-            <Typography variant="h6" color="primary">
-              Description
-            </Typography>
-            <RIETextArea
-              value={eventStore.selectedEvent.description}
-              change={({ description }) =>
-                eventStore.selectedEvent.setDescription(description)
-              }
-              propName="description"
-            />
-          </div>
-
-          <div className={classes.propertyBox}>
-            <Typography variant="h6" color="primary">
-              Location
-            </Typography>
-            <RIETextArea
-              value={eventStore.selectedEvent.location || "Enter location"}
-              change={({ location }) =>
-                eventStore.selectedEvent.setLocation(location)
-              }
-              propName="location"
-            />
-          </div>
-
-          <div className={classes.propertyBox}>
-            <Typography variant="h6" color="primary">
-              Date and Time
-            </Typography>
-            <DateAndTimePickers
-              value={eventStore.selectedEvent.dateAndTime}
-              onChange={eventStore.selectedEvent.setDateAndTime}
-            />
-          </div>
-
-          <div className={classes.propertyBox}>
-            <Typography variant="h6" color="primary">
-              Tasks
-            </Typography>
-            <div className={classes.tasks}>
-              <Tasks tasks={eventStore.selectedEvent.tasks} addTask={eventStore.selectedEvent.addTask} removeTask={eventStore.selectedEvent.removeTask} />
+        <Grid container spacing={3}>
+          <Grid container>
+            <div className="action-buttons" className={classes.actionButtons}>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  className={classes.button}
+                  color="secondary"
+                >
+                  Publish
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  className={classes.button}
+                  color="primary"
+                >
+                  Save
+                </Button>
+              </Grid>
+              <Grid item>
+                <FormDialog />
+              </Grid>
             </div>
-          </div>
+          </Grid>
 
-          <div className={classes.propertyBox}>
-            <Typography variant="h6" color="primary">
-              Playlist
-            </Typography>
-            <div className={classes.tasks}>
-              <Playlist playlist={eventStore.selectedEvent.playlist} />
-            </div>
+          <Grid item xs={12}>
+            <Divider style={{ marginTop: "16px", marginBottom: "16px" }} />
+          </Grid>
+
+          <div className={classes.eventDetails}>
+            <Grid container>
+              <Grid item xs={3}>
+                <div className={classes.propertyBox}>
+                  <Typography variant="h6" color="primary">
+                    Title
+                  </Typography>
+                  <TextField
+                    id="title"
+                    className={classes.textField}
+                    value={eventStore.selectedEvent.title}
+                    onChange={event =>
+                      eventStore.selectedEvent.setTitle(event.target.value)
+                    }
+                    placeholder="Who..."
+                  />
+                </div>
+              </Grid>
+
+              <Grid item xs={9}>
+                <div className={classes.propertyBox}>
+                  <Typography variant="h6" color="primary">
+                    Description
+                  </Typography>
+                  <TextField
+                    id="description"
+                    className={classes.textField}
+                    value={eventStore.selectedEvent.description}
+                    onChange={event =>
+                      eventStore.selectedEvent.setDescription(
+                        event.target.value
+                      )
+                    }
+                    placeholder="What..."
+                    fullWidth
+                  />
+                </div>
+              </Grid>
+
+              <Grid item xs={3}>
+                <div className={classes.propertyBox}>
+                  <Typography variant="h6" color="primary">
+                    Location
+                  </Typography>
+                  <TextField
+                    id="location"
+                    className={classes.textField}
+                    value={eventStore.selectedEvent.location}
+                    onChange={event =>
+                      eventStore.selectedEvent.setLocation(event.target.value)
+                    }
+                    placeholder="Where..."
+                  />
+                </div>
+              </Grid>
+
+              <Grid item xs={9}>
+                <div className={classes.propertyBox}>
+                  <Typography variant="h6" color="primary">
+                    Date and Time
+                  </Typography>
+                  <DateAndTimePickers
+                    value={eventStore.selectedEvent.dateAndTime}
+                    onChange={eventStore.selectedEvent.setDateAndTime}
+                  />
+                </div>
+              </Grid>
+
+              <Grid item xs={12}>
+                <div className={classes.propertyBox}>
+                  <Typography variant="h6" color="primary">
+                    Tasks
+                  </Typography>
+                  <div className={classes.tasks}>
+                    <Tasks
+                      tasks={eventStore.selectedEvent.tasks}
+                      addTask={eventStore.selectedEvent.addTask}
+                      removeTask={eventStore.selectedEvent.removeTask}
+                    />
+                  </div>
+                </div>
+              </Grid>
+
+              <Grid item xs={12}>
+                <div className={classes.propertyBox}>
+                  <Typography variant="h6" color="primary">
+                    Playlist
+                  </Typography>
+                  <div className={classes.tasks}>
+                    <Playlist playlist={eventStore.selectedEvent.playlist} />
+                  </div>
+                </div>
+              </Grid>
+            </Grid>
           </div>
-        </div>
+        </Grid>
       </div>
     );
   }
